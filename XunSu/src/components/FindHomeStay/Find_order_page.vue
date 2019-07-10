9bbb70efff562695ed9b1891fb005ad87d42425a
sean-yuan-man <meridianscyyb@126.com>
2019-07-11 01:02:07 +0800

    uncompleted order page

---
+++ XunSu/src/components/FindHomeStay/Find_order_page.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="order-container">
+        <mt-header title="订单">
+            <router-link to="/" slot="left">
+                <mt-button icon="back">返回</mt-button>
+            </router-link>
+                <mt-button icon="more" slot="right"></mt-button>
+        </mt-header>
+        <div class="order1">
+            <img src="../../assets/Orderimage/order_background_image.jpg" alt="">
+            <h4 style="color:gray">您还没有订单哦!!</h4>
+            <mt-button size="large">客观这边请</mt-button>
+        </div>
+    
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }//return 结束
+    }//data 结束
+}
+</script>
+<style scrop>
+    .order-container{
+  
+
+}
+.mint-header{
+    height: 55px;
+    width: 374px;
+    padding: 12px;
+    margin-left: -7px;
+    margin-top: -7px;
+    background-color:#3BD86C;
+}
+.mint-header-title{
+    font-size:20px;
+    color:black;
+}
+.order1{
+    margin-top:75px;
+     
+}
+.mint-button--large{
+    background-color:#3BD86C;
+    color:bisque;
+    margin-top: 50px;
+}
+    
+</style>
+
+
--- XunSu/src/router/index.js
+++ XunSu/src/router/index.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Detail from '../components/Detail.vue'
+//引入Find_order_page组件
+import Find_order_page from "../components/FindHomeStay/Find_order_page.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:'/Find_order_page',component:Find_order_page},
     {path: '/',name: 'HelloWorld',component: HelloWorld},
     {path: '/detail',name: 'Detail',component: Detail}
   ]
