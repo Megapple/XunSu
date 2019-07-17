b617ed6d25d65a40a381ab13d2fe3d2c3c7cedd7
megapple <euphoria0731@163.com>
2019-07-17 17:37:37 +0800

    ~

--- XunSu/XunSu_app/src/components/detail/DetailMap.vue
+++ XunSu/XunSu_app/src/components/detail/DetailMap.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div id="back">
+      <span class="iconfont icon-fanhuijiantou" @click="back()"></span>
+      <span v-text="title"></span>
+    </div>
     <div id="container"></div>
   </div>
 </template>
@@ -8,7 +12,7 @@
 export default {
   data() {
     return {
-      
+      title:"寻宿·达内山客栈"
     }
   },
   methods:{
@@ -17,8 +21,11 @@ export default {
       var map = new qq.maps.Map(document.getElementById("container"), {
         center: new qq.maps.LatLng(34.233170,108.905150),      // 地图的中心地理坐标
         zoom: 20,     // 地图缩放级别
-        mapStyleId: 'style1'  // 该key绑定的style1对应于经典地图样式，若未绑定将弹出无权限提示窗
+        mapStyleId: 'style 1 白浅'  // 该key绑定的style1对应于经典地图样式，若未绑定将弹出无权限提示窗
       });
+    },
+    back(){
+      this.$router.push("/Detail")
     }
   },
   mounted(){
@@ -30,7 +37,26 @@ export default {
 <style scoped>
 #container{
     min-width:375px;
-    min-height:667px;
+    min-height:627px;
+}
+#back{
+  width:100%;
+  height:40px;
+  line-height:40px;
+  /* display:flex; */
+  /* justify-content: center; */
+  background:rgba(245,156,26,1);
+  color:white;
+}
+#back>span{
+  /* width:10%; */
+  height:40px;
+  float:left;
+  margin-left:20px;
+}
+#back>span+span{
+  width:65%;
+  text-align: center;
 }
 </style>
 
