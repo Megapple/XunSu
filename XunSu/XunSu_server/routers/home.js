fac69da88e12fb22b9efde6ecaebe8c480db7813
weiwenwen <781108355@qq.com>
2019-07-26 20:49:25 +0800

    Home

--- XunSu/XunSu_server/routers/home.js
+++ XunSu/XunSu_server/routers/home.js
@@ -2,10 +2,29 @@ const express=require("express");
 const router=express.Router();
 const pool=require("../pool");
 router.get("/",(req,res)=>{
-  var sql=`SELECT * FROM leaseroom`;
-  pool.query(sql,[],(err,result)=>{
-     if(err) throw (err);
-     res.send(result)
-  })
+//     var output = {
+//         leaseroom: [],
+//         homePic: []
+//       }
+//   var sql=`SELECT * FROM leaseroom`;
+//   pool.query(sql,[],(err,result)=>{     
+//       console.log(result)
+//      if(err) throw (err);
+//      output.leaseroom = result;
+//      var lid = output.leaseroom.lid;
+//      var sql2=`SELECT * FROM WHERE homeid=?`
+//      pool.query(sql2,[lid],(err,result=>{
+//          if(err)throw err;
+//          this.homePic=result;
+//          res.send(result)
+//          return;
+//      }))
+//   })
+    var sql=`SELECT * FROM leaseroom`;
+    pool.query(sql,[],(err,result)=>{
+      if(err) throw(err);
+        // res.send({code:0});
+          res.send(result);
+    })
 })
 module.exports=router;
