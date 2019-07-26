5405cd87168c1c0cd930e45a7e7dd7c2feb793ba
Sean Yuan <meridianscyyb@126.com>
2019-07-26 19:18:17 +0800

    temporary

--- XunSu/XunSu_server/routers/add.js
+++ XunSu/XunSu_server/routers/add.js
@@ -2,16 +2,24 @@ const express=require("express");
 const router=express.Router();
 const pool=require("../pool");
 //添加入住人信息
-
-router.post("/AddMsg",(req,res)=>{
-    var sql="INSERT INTO 'add_guest_msg' ('id','name','phone','credentials','cardid')";
-pool.query(sql,[id,name,phone,credentials,cardid],(result,err)=>{
+console.log(pool);
+router.get("/Add",(req,res)=>{
+    console.log(pool);
+    var id=req.body.id;
+    var name=req.body.name;
+    var phone=req.body.phone;
+    var credentials=req.body.credentials;
+    var cardid=req.body.cardid;
+    var sql="INSERT INTO add_guest_msg VALUES ('NULL','123','151615','1','5')";
+    pool.query(sql,(result,err)=>{
     if(err) throw err;
+    console.log(result);
     if(result.affectedRows>0){
         res.send({code:200,msg:"添加成功"})
     }else{
         res.send({code:401,mgs :"添加失败"})
     }
+   
 })
 })
 
