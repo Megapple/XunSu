889ba20690dddd53b538a56030c38a3e1eda2b7b
xingxingyu008 <263352053@qq.com>
2019-07-27 01:09:38 +0800

    home更改

--- XunSu/XunSu_server/routers/home.js
+++ XunSu/XunSu_server/routers/home.js
@@ -1,11 +1,11 @@
 const express=require("express");
 const router=express.Router();
 const pool=require("../pool");
-router.get("/",(req,res)=>{
-//     var output = {
-//         leaseroom: [],
-//         homePic: []
-//       }
+// router.get("/",(req,res)=>{
+//      var output = {
+//          leaseroom: [],
+//          user: []
+//        }
 //   var sql=`SELECT * FROM leaseroom`;
 //   pool.query(sql,[],(err,result)=>{     
 //       console.log(result)
@@ -20,11 +20,38 @@ router.get("/",(req,res)=>{
 //          return;
 //      }))
 //   })
-    var sql=`SELECT * FROM leaseroom`;
-    pool.query(sql,[],(err,result)=>{
-      if(err) throw(err);
-        // res.send({code:0});
-          res.send(result);
+//     var sql=`SELECT * FROM leaseroom`;
+//     pool.query(sql,[],(err,result)=>{
+//       if(err) throw(err);
+//         output.leaseroom=result;
+//         // res.send({code:0});
+//           res.send(result);
+//     })
+// })
+router.get('/', function (req, res) {
+  var output = {
+    user: [],
+    leaseroom: [],
+    homePic: []
+  }
+    var sql1 = `select * from users`;
+    pool.query(sql1, (err, result) => {
+      if (err) console.log(err);
+      output.user = result;
+      console.log(output.user);
+      var sql2 = `select * from leaseroom`;
+      pool.query(sql2, (err, result) => {
+        if (err) console.log(err);
+        output.leaseroom = result;
+          var sql3 = `select imgurl from homePic`;
+          pool.query(sql3, (err, result) => {
+            if (err) console.log(err);
+            output.homePic=result;
+            res.send({ code: 200, msg: output });
+          })
+      })
     })
-})
+
+});
+
 module.exports=router;
